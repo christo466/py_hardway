6237f44377776693405f743904759c978776d235
christo466 <christoaj466@gmail.com>
2024-05-22 21:24:25 +0530

    exercise 7 - Added comments to explain each print statement in the nursery rhyme. - Detailed the usage of the format method to insert 'snow' into the string. - Explained the purpose of multiplying the string to print ten periods. - Described the assignment of individual characters to variables. - Clarified the concatenation and printing of characters to form "Cheese" and "Burger" on the same line.

---
+++ ex7.py
@@ -0,0 +1,32 @@
+# Print the first line of the nursery rhyme.
+print("Mary had a little lamb.")
+
+# Print the second line, using the format method to insert 'snow' into the string.
+print("Its fleece was white as {}.".format('snow'))
+
+# Print the third line of the nursery rhyme.
+print("And everywhere that Mary went.")
+
+# Print ten periods in a row to create a dotted line.
+print("." * 10)
+
+# Define individual characters for the words "Cheese" and "Burger".
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Concatenate and print the characters to form the word "Cheese".
+# The end=' ' argument ensures that there is a space instead of a newline at the end.
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+
+# Concatenate and print the characters to form the word "Burger" on the same line.
+print(end7 + end8 + end9 + end10 + end11 + end12)
