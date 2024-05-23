26b94e5b66dbb991652d391ccdc9c04ef551a076
christo466 <christoaj466@gmail.com>
2024-05-23 08:24:25 +0530

    exercise 12 - Prompt the user to enter their age, height, and weight in a more concise manner. - Use `input()` with inline prompts for age, height, and weight. - Print a formatted summary of the collected user information using f-strings

---
+++ ex12.py
@@ -0,0 +1,11 @@
+# Prompt the user to enter their age, and capture the input in the 'age' variable
+age = input("How old are you? ")
+
+# Prompt the user to enter their height, and capture the input in the 'height' variable
+height = input("How tall are you? ")
+
+# Prompt the user to enter their weight, and capture the input in the 'weight' variable
+weight = input("How much do you weigh? ")
+
+# Print a formatted string that includes the user's age, height, and weight
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
