4c16f15fd55cf3b91c322791a3d0659e6fb7d402
christo466 <christoaj466@gmail.com>
2024-05-30 19:00:37 +0530

    exercise 29 - Initialize variables to represent the number of people, cats, and dogs. - Add conditional checks to compare the number of people with the number of cats and dogs. - Print appropriate messages based on the comparisons. - Increment the number of dogs and re-evaluate conditions to print updated messages.

---
+++ ex29.py
@@ -0,0 +1,42 @@
+# Initialize variables to represent the number of people, cats, and dogs
+people = 20
+cats = 30
+dogs = 15
+
+# Check if the number of people is less than the number of cats
+if people < cats:   
+    # Print a warning message if there are more cats than people
+    print("Too many cats! The world is doomed!")
+
+# Check if the number of people is greater than the number of cats
+if people > cats:    
+    # Print a positive message if there are fewer cats than people
+    print("Not many cats! The world is saved!")
+
+# Check if the number of people is less than the number of dogs
+if people < dogs:
+    # Print a humorous message if there are more dogs than people
+    print("The world is drooled on!")
+
+# Check if the number of people is greater than the number of dogs
+if people > dogs: 
+    # Print a positive message if there are fewer dogs than people
+    print("The world is dry!")
+
+# Increase the number of dogs by 5
+dogs += 5
+
+# Check if the number of people is greater than or equal to the number of dogs
+if people >= dogs:
+    # Print a message indicating that the number of people is greater than or equal to the number of dogs
+    print("People are greater than or equal to dogs.")
+
+# Check if the number of people is less than or equal to the number of dogs
+if people <= dogs:
+    # Print a message indicating that the number of people is less than or equal to the number of dogs
+    print("People are less than or equal to dogs.")
+
+# Check if the number of people is equal to the number of dogs
+if people == dogs:
+    # Print a humorous message if the number of people equals the number of dogs
+    print("People are dogs.")
